29ce1e177c339364286ae044c52f39b1a1b120e1
csantillgar <csantill@myuax.com>
2023-12-13 19:54:31 +0100

    ejercicio1

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
+struct Estudiante {
+    char nombre[30];
+    int edad;
+    float promedio;
+};
+void mostrarEstudiante(struct Estudiante *estudiante) {
+    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", estudiante->nombre, estudiante->edad, estudiante->promedio);
+}
+
+
+
+
 int main() {
 
-    struct Estudiante {
-        char nombre[30];
-        int edad;
-        float promedio;
-    };
+return 0;
 }
