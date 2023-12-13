0c505a7758928117240c6fe2621014628aed4a5b
csantillgar <csantill@myuax.com>
2023-12-13 21:30:19 +0100

    Ejercicio5a

--- Ejercicio 5a/main.cpp
+++ Ejercicio 5a/main.cpp
@@ -1,4 +1,12 @@
 #include<iostream>
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float nota;
+};
+void imprimirEstudiantePorValor(struct Estudiante est) {
+    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est.nombre, est.edad, est.promedio);
+}
 int main()
 {
     std::cout << "Hello World!" << std::endl;
