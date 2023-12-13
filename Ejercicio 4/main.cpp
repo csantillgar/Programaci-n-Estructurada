7f354059fccb67847156ecc801df60ed61684392
csantillgar <csantill@myuax.com>
2023-12-13 21:19:22 +0100

    Ejercicio4

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <string>
+#include <cstring>
 
 struct Estudiante {
     char nombre[50];
@@ -9,14 +9,16 @@ struct Estudiante {
 typedef struct Estudiante Estudiante;
 
 
-
-
-
-
-
 int main()
 {
     Estudiante estudiante3;
+
+    strcpy(estudiante3.nombre, "Pedro");
+    estudiante3.edad = 25;
+    estudiante3.promedio = 9.7;
+
+    // Mostrar la información del estudiante utilizando el alias
+    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", estudiante3.nombre, estudiante3.edad, estudiante3.promedio);
 
 
 
