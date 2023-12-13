44431cc9f4c8ca4cf60814a3b1957a2b14760c22
csantillgar <csantill@myuax.com>
2023-12-13 21:05:28 +0100

    Ejercicio3

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
-#include <string>
+#include <cstring>
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float nota;
+};
+
+struct Estudiante copiarEstudiante(struct Estudiante estudiante) {
+    struct Estudiante nuevoEstudiante;
+    strcpy(nuevoEstudiante.nombre, estudiante.nombre);
+    nuevoEstudiante.edad = estudiante.edad;
+    nuevoEstudiante.nota = estudiante.nota;
+    return nuevoEstudiante;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
     std::string name;
