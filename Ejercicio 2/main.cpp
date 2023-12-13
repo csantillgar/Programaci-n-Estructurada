6d2577e92a7c54cfbb5396bc70e152b1dc685c3a
csantillgar <csantill@myuax.com>
2023-12-13 20:54:53 +0100

    Ejercicio2

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -11,7 +11,10 @@ int main()
     union Dato dato;
     dato.i = 10;
     printf("dato.i = %d\n", dato.i);
-
+    dato.f = 220.5;
+    printf("dato.f = %f\n", dato.f);
+    strcpy(dato.str, "C++ Programming");
+    printf("dato.str = %s\n", dato.str);
 
 
 
