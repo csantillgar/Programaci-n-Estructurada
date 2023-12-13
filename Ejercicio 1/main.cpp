6225b87f60decd6d30a7dfebfd2fe7a4687cb237
csantillgar <csantill@myuax.com>
2023-12-13 20:01:21 +0100

    ejercicio1

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -8,7 +8,23 @@ struct Estudiante {
 void mostrarEstudiante(struct Estudiante *estudiante) {
     printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", estudiante->nombre, estudiante->edad, estudiante->promedio);
 }
+void agregarEstudiante(struct Estudiante *lista, int *numEstudiantes) {
+    // Verifica si hay espacio en la lista (por ejemplo, un límite de 10 estudiantes)
+    if (*numEstudiantes < 10) {
+        printf("Ingrese el nombre del estudiante: ");
+        scanf("%s", lista[*numEstudiantes].nombre);
+        printf("Ingrese la edad del estudiante: ");
+        scanf("%d", &lista[*numEstudiantes].edad);
+        printf("Ingrese el promedio del estudiante: ");
+        scanf("%f", &lista[*numEstudiantes].promedio);
 
+        // Incrementa el contador de estudiantes en la lista
+        (*numEstudiantes)++;
+        printf("Estudiante agregado correctamente.\n");
+    } else {
+        printf("La lista de estudiantes está llena. No se pueden agregar más estudiantes.\n");
+    }
+}
 
 
 
